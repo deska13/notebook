309baaeb5adbf7c524ac6c392d6ca964dda38d1b
deska13 <deskakuzmin1999@yandex.ru>
2022-03-02 22:15:17 +0500

    .

--- app.py
+++ app.py
@@ -16,5 +16,15 @@ def get_categories():
 
 
 @app.delete('/delete_category/category_id={category_id}')
-def delete_category(category_id):
+def delete_category(category_id: int):
     return db_query.delete_category_by_id(category_id)
+
+
+@app.post('/add_note/category_id={category_id}&&title={title}&&content={content}')
+def add_note(category_id: int, title: str, content: str):
+    return db_query.add_note(category_id, title, content)
+
+
+@app.get('/get_note/note_id={note_id}')
+def get_note(note_id: int):
+    return db_query.get_note_by_id(note_id)
--- orm/queries/note.py
+++ orm/queries/note.py
@@ -1,13 +1,41 @@
+import traceback
 from ..models import *
 from .engine import session
 
 
-def add_note():
-    return
+def add_note(category_id, title, content):
+    try:
+        session.query(Category).filter(Category.id == category_id).one()
+    except:
+        return ("Неверный id категории")
+    
+    note = Note(
+        category_id=category_id,
+        title=title,
+        content=content
+    )
+    session.add(note)
+    session.commit()
+    return note.id
 
 
-def get_note():
-    return
+def get_note_by_id(note_id):
+    try:
+        rows = session.query(
+            Note,
+            Category
+        ).filter(
+            Note.id == note_id,
+            Note.category_id == Category.id
+            ).all()
+        for row in rows:
+            note, category = row
+            print(vars(note))
+            print(vars(category))
+    except:
+        traceback.print_exc()
+        return ("Неверный id категории")
+
 
 
 def delete_note():
